b19e8977724edef26326a0b35c51b0dbea4d7aa8
Ruslan <117785539+Guch1g0v@users.noreply.github.com>
2024-07-29 11:43:57 +0500

    Update vite.config2.ts

--- vite.config2.ts
+++ vite.config2.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { createHtmlPlugin } from 'vite-plugin-html'
+import svgr from 'vite-plugin-svgr'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -14,6 +16,7 @@ export default defineConfig({
       },
       template: './index.html',
     }),
+    svgr()
   ],
   build: {
     rollupOptions: {
